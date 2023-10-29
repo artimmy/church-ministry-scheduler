784667074d6201d12d05a5fd64e494891faaac81
artimmy <arthurtimmy@hotmail.com>
2023-10-29 15:25:03 -0300

    setup main

---
+++ lib/app/app_widget.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'modules/home/presenter/pages/home_page.dart';
+
+class AppWidget extends StatefulWidget {
+  const AppWidget({super.key});
+
+  @override
+  State<AppWidget> createState() => _AppWidgetState();
+}
+
+class _AppWidgetState extends State<AppWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
